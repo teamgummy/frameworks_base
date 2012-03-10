8884a03120e3d0d2e5ffdfeb55178dd75a1ed8a0
Kejar31 <justin.a.rogers@gmail.com>
2012-03-09 23:48:34 -0600

    Don't enable the light sensor if it's not being used We want to fully disable the lightsensor when it's not used, for two reasons: 1 - Power savings 2 - Sensors that control the backlight directly need to be disabled when brightness control is set to manual. Forcibly enabling the sensor when the screen turns on makes these devices incapable of using manual brightness.
    Change-Id: I64c1114348ff096e0a72b265f6f141d1ecef249c
    
    Fix button backlights that depend on ALS
    
    On a few devices, lighting the buttons when the screen turns on
    depends on the lightsensor, regardless of the user preference.
    So go back to enabling the sensor whenever the screen turns on, and
    enforce the user preference after running through the light settings
    once.
    
    Change-Id: I6f036a32315eb7688a9cd11d4bd70dbca1b2fa8b

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -51,6 +51,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.WorkSource;
+import android.os.SystemProperties;
 import android.provider.Settings.SettingNotFoundException;
 import android.provider.Settings;
 import android.util.EventLog;
@@ -244,7 +245,7 @@ public class PowerManagerService extends IPowerManager.Stub
     private int mButtonBrightnessOverride = -1;
     private int mScreenBrightnessDim;
     private boolean mUseSoftwareAutoBrightness;
-    private boolean mAutoBrightessEnabled;
+    private boolean mAutoBrightessEnabled = true;
     private int[] mAutoBrightnessLevels;
     private int[] mLcdBacklightValues;
     private int[] mButtonBacklightValues;
@@ -1711,6 +1712,13 @@ public class PowerManagerService extends IPowerManager.Stub
                     mLightSensorValue = -1;
                     // reset our highest light sensor value when the screen turns off
                     mHighestLightSensorValue = -1;
+                }
+                else if (!mAutoBrightessEnabled && SystemProperties.getBoolean(
+                    "ro.hardware.respect_als", false)) {
+                    /* Force a light sensor reset since we enabled it
+                       when the screen came on */
+                    mAutoBrightessEnabled = true;
+                    setScreenBrightnessMode(Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
                 }
             }
         }
