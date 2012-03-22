9c55d97616545866b1ccd72a802073716c48cc75
Dustin Einglett <x13thangelx@gmail.com>
2012-03-22 03:20:30 -0500

    add hdmi out audio fix thanks to SaurusX

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -144,6 +144,10 @@ public class AudioService extends IAudioService.Stub {
     private Object mSettingsLock = new Object();
     private boolean mMediaServerOk;
 
+    private static final String MAIN_MIC_CHOICE = "omap.audio.mic.main";
+    private static final String SUB_MIC_CHOICE = "omap.audio.mic.sub";
+    private static final string EXTDISP_STATUS_DISPLAY = "com.motorola.intent.action.EXTDISP_STATUS_DISPLAY";
+
     private SoundPool mSoundPool;
     private Object mSoundEffectsLock = new Object();
     private static final int NUM_SOUNDPOOL_CHANNELS = 4;
@@ -374,6 +378,10 @@ public class AudioService extends IAudioService.Stub {
         intentFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
         intentFilter.addAction(Intent.ACTION_SCREEN_ON);
         intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        // See if this works as in GB to fix hdmi audio out
+        intentFilter.addAction(BluetoothA2dp.ACTION_SINK_STATE_CHANGED);
+        intentFilter.addAction(BluetoothHeadset.ACTION_STATE_CHANGED);
+        intentFilter.addAction(EXTDISP_STATUS_DISPLAY);
         context.registerReceiver(mReceiver, intentFilter);
 
         // Register for package removal intent broadcasts for media button receiver persistence
@@ -2595,6 +2603,23 @@ public class AudioService extends IAudioService.Stub {
                         mConnectedDevices.put(
                                 new Integer(AudioSystem.DEVICE_OUT_ANLG_DOCK_HEADSET), "");
                     }
+                }
+            } else if (action.equals(EXTDISP_STATUS_DISPLAY)) {
+            	Log.v(TAG,"Broadcast Receiver: Got action EXTDISP_STATUS_DISPLAY");
+                boolean audio = intent.getBooleanExtra("audio", false);
+                boolean isConnected = mConnectedDevices.containsKey(AudioSystem.DEVICE_OUT_HDMI);
+                if (!audio && isConnected) {
+                    Log.v(TAG,"Broadcast Receiver: For HDMI Audio State = 0 && isConnected = TRUE");
+                    AudioSystem.setDeviceConnectionState(AudioSystem.DEVICE_OUT_HDMI,
+                            AudioSystem.DEVICE_STATE_UNAVAILABLE,
+                            "");
+                    mConnectedDevices.remove(AudioSystem.DEVICE_OUT_HDMI);
+                } else if ((audio != true) || isConnected)  {
+                    Log.v(TAG,"Broadcast Receiver: For HDMI Audio State = 0 && isConnected = FALSE");
+                    AudioSystem.setDeviceConnectionState(AudioSystem.DEVICE_OUT_HDMI,
+                            AudioSystem.DEVICE_STATE_AVAILABLE,
+                            "");
+                    mConnectedDevices.put( new Integer(AudioSystem.DEVICE_OUT_HDMI), "");
                 }
             } else if (action.equals(Intent.ACTION_HDMI_AUDIO_PLUG)) {
                 int state = intent.getIntExtra("state", 0);
