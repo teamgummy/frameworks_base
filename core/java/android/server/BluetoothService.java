cf4b32d5b9f712ff027d093d8255e934a079cae1
Anatolii.Shuba <x0158321@ti.com>
2012-04-03 16:22:11 +0300

    Bluetooth: Fixed memory leak and file handles leak
    After next pairing operation with same Bluetooth device
    previous BT state machine never stopped, but new one is created.
    
    Change-Id: I2e9eccd678afaba19770b5ba56dfa5fa0b9487de
    Signed-off-by: Anatolii.Shuba <x0158321@ti.com>
    
    Conflicts:
    
    	core/java/android/server/BluetoothService.java
    
    Conflicts:
    
    	core/java/android/server/BluetoothService.java

--- core/java/android/server/BluetoothService.java
+++ core/java/android/server/BluetoothService.java
@@ -2392,7 +2392,15 @@ public class BluetoothService extends IBluetooth.Stub {
     BluetoothDeviceProfileState addProfileState(String address, boolean setTrust) {
         BluetoothDeviceProfileState state =
             new BluetoothDeviceProfileState(mContext, address, this, mA2dpService, setTrust);
-        mDeviceProfileState.put(address, state);
+        if (SystemProperties.OMAP_ENHANCEMENT) {
+            BluetoothDeviceProfileState oldStateMachine  = mDeviceProfileState.put(address, state);
+            if (oldStateMachine != null) {
+                oldStateMachine.quit();
+                oldStateMachine = null;
+            }
+        } else {
+            mDeviceProfileState.put(address, state);
+        }
         state.start();
         return state;
     }
