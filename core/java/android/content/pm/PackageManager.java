AOKP/frameworks_base_disabled

Update variable names
Change-Id: Ife0a1a4ac781cf6000f0d4048f4c8fe9c1906b05

core/java/android/content/pm/PackageManager.java
@@ -763,14 +763,14 @@ public abstract class PackageManager {
     * Feature for {@link #getSystemAvailableFeatures} and
     * {@link #hasSystemFeature}: The device has a keyboard dock.
     */
-    public static final String FEATURE_EOS_KB_DOCK = "eos.hardware.dock";
+    public static final String FEATURE_TF101_KB_DOCK = "aokp.hardware.dock";
 
     /**
     * Feature for {@link #getSystemAvailableFeatures} and
     * {@link #hasSystemFeature}: The device uses a hall effect
     * sensor to change the value of the lid switch.
     */
-    public static final String FEATURE_EOS_HALL_SENSOR = "eos.hardware.hall_sensor";
+    public static final String FEATURE_TF101_HALL_SENSOR = "aokp.hardware.hall_sensor";
 
     /**
      * Feature for {@link #getSystemAvailableFeatures} and {@link #hasSystemFeature}: The device's

teamgummy/frameworks_base

core/java/android/content/pm/PackageManager.java
@@ -763,14 +763,14 @@ public abstract class PackageManager {
     * Feature for {@link #getSystemAvailableFeatures} and
     * {@link #hasSystemFeature}: The device has a keyboard dock.
     */
-    public static final String FEATURE_EOS_KB_DOCK = "eos.hardware.dock";
+    public static final String FEATURE_TF101_KB_DOCK = "asus.hardware.dock";
 
     /**
     * Feature for {@link #getSystemAvailableFeatures} and
     * {@link #hasSystemFeature}: The device uses a hall effect
     * sensor to change the value of the lid switch.
     */
-    public static final String FEATURE_EOS_HALL_SENSOR = "eos.hardware.hall_sensor";
+    public static final String FEATURE_TF101_HALL_SENSOR = "asus.hardware.hall_sensor";
 
     /**
      * Feature for {@link #getSystemAvailableFeatures} and {@link #hasSystemFeature}: The device's