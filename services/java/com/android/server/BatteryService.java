4180e49b6cc50d91ac581c32ce87312eb1b51f45
Dustin Einglett <x13thangelx@gmail.com>
2012-05-03 14:18:56 -0500

    Revert "Add support for external dock batterys"
    This reverts commit d9d488dc3f468fe8dd49969d6f83f63bc576bd72.
    
    Conflicts:
    
    	core/res/res/values/config.xml

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -105,10 +105,6 @@ class BatteryService extends Binder {
     private boolean mBatteryLevelCritical;
     private int mInvalidCharger;
 
-    private int mDockBatteryStatus;
-    private int mDockBatteryLevel;
-    private String mDockBatteryPresent;
-
     private int mLastBatteryStatus;
     private int mLastBatteryHealth;
     private boolean mLastBatteryPresent;
@@ -120,8 +116,6 @@ class BatteryService extends Binder {
 
     private int mLowBatteryWarningLevel;
     private int mLowBatteryCloseWarningLevel;
-
-    private boolean mHasDockBattery;
 
     private int mPlugType;
     private int mLastPlugType = -1; // Extra state so we can detect first run
@@ -145,9 +139,6 @@ class BatteryService extends Binder {
                 com.android.internal.R.integer.config_lowBatteryWarningLevel);
         mLowBatteryCloseWarningLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_lowBatteryCloseWarningLevel);
-
-        mHasDockBattery = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_hasDockBattery);
 
         mPowerSupplyObserver.startObserving("SUBSYSTEM=power_supply");
 
@@ -423,12 +414,6 @@ class BatteryService extends Binder {
         intent.putExtra(BatteryManager.EXTRA_TEMPERATURE, mBatteryTemperature);
         intent.putExtra(BatteryManager.EXTRA_TECHNOLOGY, mBatteryTechnology);
         intent.putExtra(BatteryManager.EXTRA_INVALID_CHARGER, mInvalidCharger);
-
-        if (mHasDockBattery){
-            intent.putExtra(BatteryManager.EXTRA_DOCK_STATUS, mDockBatteryStatus);
-            intent.putExtra(BatteryManager.EXTRA_DOCK_LEVEL, mDockBatteryLevel);
-            intent.putExtra(BatteryManager.EXTRA_DOCK_AC_ONLINE, false);
-        }
 
         if (false) {
             Slog.d(TAG, "level:" + mBatteryLevel +
