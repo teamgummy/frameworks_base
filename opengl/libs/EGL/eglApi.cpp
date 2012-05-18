c46b9975133fbbbf6bd84e2857a8cbe7c7e4fb8c
Jonathan Bither <jonbither@gmail.com>
2012-05-18 11:17:52 -0400

    EGL: Fix reporting of incorrect extension string
    When asking EGL for it's extension string it will return the string from
    the Android software implementation and not the string associated with
    the GPU thereby preventing well behaving software from utilizing such
    features as GPU fence sync objects.
    
    Change-Id: I78ed7a94d4322164331e52ab08c9b8c6163a3c40
    
    Signed-off-by: Christian Bejram <christian.bejram@stericsson.com>

--- opengl/libs/EGL/eglApi.cpp
+++ opengl/libs/EGL/eglApi.cpp
@@ -980,6 +980,9 @@ const char* eglQueryString(EGLDisplay dpy, EGLint name)
         case EGL_VERSION:
             return dp->getVersionString();
         case EGL_EXTENSIONS:
+            if (dp->disp[IMPL_HARDWARE].queryString.extensions) {
+                return dp->disp[IMPL_HARDWARE].queryString.extensions;
+            }
             return dp->getExtensionString();
         case EGL_CLIENT_APIS:
             return dp->getClientApiString();
