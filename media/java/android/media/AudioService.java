b82f2350113766f2748085ac88f2e14e817e5d1b
Dustin Einglett <x13thangelx@gmail.com>
2012-05-24 15:35:53 -0500

    Revert "Only play sound effects if stream is not muted."
    This reverts commit 7b7b2ffa343c8d969eb1d290334a5c6b64b7ebc1.

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -2298,9 +2298,7 @@ public class AudioService extends IAudioService.Stub {
                     break;
 
                 case MSG_PLAY_SOUND_EFFECT:
-                    if (!isStreamMute(AudioSystem.STREAM_SYSTEM)) {
-                        playSoundEffect(msg.arg1, msg.arg2);
-                    }
+                    playSoundEffect(msg.arg1, msg.arg2);
                     break;
 
                 case MSG_BTA2DP_DOCK_TIMEOUT:
