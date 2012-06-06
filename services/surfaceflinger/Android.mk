876a334dfcf07977bd1565006a1a2612658985c0
Jonathan Bither <jonbither@gmail.com>
2012-06-06 15:50:46 -0400

    omapzoom surfaceflinger updates
    Conflicts:
    
    	services/surfaceflinger/Android.mk
    	services/surfaceflinger/Layer.cpp
    	services/surfaceflinger/LayerBase.h

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -1,6 +1,17 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(MAKECMDGOALS), sdk_addon)
+ifeq ($(TARGET_PRODUCT), s3d)
+OMAP_ENHANCEMENT_S3D := true
+LOCAL_CPPFLAGS += -DOMAP_ENHANCEMENT_S3D
+endif
+endif
+
+ifeq ($(BOARD_HAVE_CODEC_SUPPORT),SAMSUNG_CODEC_SUPPORT)
+LOCAL_CFLAGS     += -DSAMSUNG_CODEC_SUPPORT
+endif
+
 LOCAL_SRC_FILES:= \
     Layer.cpp 								\
     LayerBase.cpp 							\
@@ -15,6 +26,14 @@ LOCAL_SRC_FILES:= \
     SurfaceFlinger.cpp 						\
     SurfaceTextureLayer.cpp 				\
     Transform.cpp
+
+ifdef OMAP_ENHANCEMENT_S3D
+LOCAL_SRC_FILES += \
+    S3DSurfaceFlinger.cpp                   \
+    OmapLayer.cpp                           \
+    OmapLayerScreenshot.cpp                 \
+    DisplayHardware/S3DHardware.cpp
+endif
 
 LOCAL_CFLAGS:= -DLOG_TAG=\"SurfaceFlinger\"
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
--- services/surfaceflinger/LayerBase.h
+++ services/surfaceflinger/LayerBase.h
@@ -235,6 +235,9 @@ protected:
           void clearWithOpenGL(const Region& clip, GLclampf r, GLclampf g,
                                GLclampf b, GLclampf alpha) const;
           void clearWithOpenGL(const Region& clip) const;
+#ifdef OMAP_ENHANCEMENT_S3D
+          virtual void drawWithOpenGL(const Region& clip) const;
+#else
           void drawWithOpenGL(const Region& clip) const;
 
           void setFiltering(bool filtering);
