dc686172d82f9e01fdbf90f12e5d7308226a67e3
Catherine Liu <wlcl05@motorola.com>
2012-06-29 16:09:35 -0500

    Fix code problem in writePidDns
    When app call startUsingNetworkFeature() to keep alive, there is a
    code problem in writePidDns() to update the net.dnsX.<pid> property.
    
    Change-Id: I83b02da4808f106c9ca00e350ad38e4bd5cba689

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -2130,8 +2130,9 @@ private NetworkStateTracker makeWimaxStateTracker() {
             String dnsString = dns.getHostAddress();
             if (changed || !dnsString.equals(SystemProperties.get("net.dns" + j + "." + pid))) {
                 changed = true;
-                SystemProperties.set("net.dns" + j++ + "." + pid, dns.getHostAddress());
+                SystemProperties.set("net.dns" + j + "." + pid, dns.getHostAddress());
             }
+            j++;
         }
         return changed;
     }
