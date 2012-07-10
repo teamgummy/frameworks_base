3d50d734bbef87e149d903bb3ed77710469a8c55
Jonathan Bither <jonbither@gmail.com>
2012-07-10 12:00:57 -0400

    GLES2: we no longer need our external hooks with our new pvr modules
    Conflicts:
    
    	opengl/libs/GLES2/gl2.cpp

--- opengl/libs/Android.mk
+++ opengl/libs/Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
 
-FLTO_FLAG=$(call cc-option,"-flto", )
-
 ###############################################################################
 # Build META EGL library
 #
@@ -19,9 +17,8 @@ LOCAL_SRC_FILES:= 	       \
 	EGL/getProcAddress.cpp.arm \
 	EGL/Loader.cpp 	       \
 
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
 LOCAL_SHARED_LIBRARIES += libcutils libutils libGLESv2_dbg
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libEGL
 LOCAL_LDFLAGS += -Wl,--exclude-libs=ALL
 LOCAL_SHARED_LIBRARIES += libdl
@@ -80,8 +77,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := GLES_CM/gl.cpp.arm
 
 LOCAL_SHARED_LIBRARIES += libcutils libEGL
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -95,10 +91,6 @@ LOCAL_CFLAGS += -DLOG_TAG=\"libGLESv1\"
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
 LOCAL_CFLAGS += -fvisibility=hidden
 
-ifeq ($(TARGET_MISSING_EGL_EXTERNAL_IMAGE),true)
-  LOCAL_CFLAGS += -DHOOK_MISSING_EGL_EXTERNAL_IMAGE
-endif
-
 include $(BUILD_SHARED_LIBRARY)
 
 
@@ -110,9 +102,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := GLES2/gl2.cpp.arm
 
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
 LOCAL_SHARED_LIBRARIES += libcutils libEGL
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libGLESv2
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -125,10 +116,6 @@ LOCAL_C_INCLUDES += bionic/libc/private
 LOCAL_CFLAGS += -DLOG_TAG=\"libGLESv2\"
 LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
 LOCAL_CFLAGS += -fvisibility=hidden
-
-ifeq ($(TARGET_MISSING_EGL_EXTERNAL_IMAGE),true)
-  LOCAL_CFLAGS += -DHOOK_MISSING_EGL_EXTERNAL_IMAGE
-endif
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -153,8 +140,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := ETC1/etc1.cpp
 
-LOCAL_CFLAGS += $(FLTO_FLAG) -ffast-math
-LOCAL_LDLIBS := $(FLTO_FLAG) -lpthread -ldl
+LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libETC1
 
 include $(BUILD_SHARED_LIBRARY)
