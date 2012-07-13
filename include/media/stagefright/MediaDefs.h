7dede412d3cb3a201c8fa621f1324fa49496da8d
Jonathan Bither <jonbither@gmail.com>
2012-07-13 18:45:26 -0400

    OMX: hand-merge all the core components of OMX from omapzoom
    Conflicts:
    
    	include/media/stagefright/MediaDefs.h
    	include/media/stagefright/OMXCodec.h
    	media/libstagefright/OMXCodec.cpp

--- include/media/stagefright/MediaDefs.h
+++ include/media/stagefright/MediaDefs.h
@@ -55,6 +55,14 @@ extern const char *MEDIA_MIMETYPE_CONTAINER_WVM;
 
 extern const char *MEDIA_MIMETYPE_TEXT_3GPP;
 
+
+#ifdef OMAP_ENHANCEMENT
+extern const char *MEDIA_MIMETYPE_CONTAINER_ASF;
+extern const char *MEDIA_MIMETYPE_VIDEO_WMV;
+extern const char *MEDIA_MIMETYPE_AUDIO_WMA;
+extern const char *MEDIA_MIMETYPE_AUDIO_WMAPRO;
+extern const char *MEDIA_MIMETYPE_AUDIO_WMALSL;
+#endif
 }  // namespace android
 
 #endif  // MEDIA_DEFS_H_
