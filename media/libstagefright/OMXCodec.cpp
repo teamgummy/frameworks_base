433a3b8ad712f6917de06a55367d2ec313d48c94
Jonathan Bither <jonbither@gmail.com>
2012-07-13 03:45:49 -0400

    OMX: remove legacy work-around

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -498,22 +498,6 @@ sp<MediaSource> OMXCodec::Create(
 
         uint32_t quirks = getComponentQuirks(componentNameBase, createEncoder);
 
-//#ifdef OMAP_COMPAT
-        if (!strcmp(componentName, "OMX.TI.Video.Decoder")) {
-            int32_t width, height;
-            bool success = meta->findInt32(kKeyWidth, &width);
-            success = success && meta->findInt32(kKeyHeight, &height);
-            CHECK(success);
-            // We need this for 720p video without AVC profile
-            // Not a good solution, but ..
-            if (width*height > 412800) {  //860*480
-               componentName = "OMX.TI.720P.Decoder";
-               LOGE("Format exceed the decoder's capabilities. %d", width*height);
-               continue;
-            }
-        }
-//#endif
-
         if (!createEncoder
                 && (quirks & kOutputBuffersAreUnreadable)
                 && (flags & kClientNeedsFramebuffer)) {
