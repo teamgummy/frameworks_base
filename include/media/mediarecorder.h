d358aa75242b5e7e0e80d58380e3303c85fda92c
Jonathan Bither <jonbither@gmail.com>
2012-07-16 16:17:08 -0400

    mediarecorder enums update
    Conflicts:
    
    	include/media/mediarecorder.h

--- include/media/mediarecorder.h
+++ include/media/mediarecorder.h
@@ -67,6 +67,11 @@ enum output_format {
     /* H.264/AAC data encapsulated in MPEG2/TS */
     OUTPUT_FORMAT_MPEG2TS = 8,
 
+
+#ifdef OMAP_ENHANCEMENT
+    /* H.264/AAC data encapsulated in RTP/MPEG2TS. Added for wifi display feature */
+    OUTPUT_FORMAT_RTP_MPEG2TS = 9,
+#endif
     OUTPUT_FORMAT_LIST_END // must be last - used to validate format type
 };
 
@@ -78,6 +83,9 @@ enum audio_encoder {
     AUDIO_ENCODER_AAC_PLUS = 4,
     AUDIO_ENCODER_EAAC_PLUS = 5,
 
+#ifdef OMAP_ENHANCEMENT
+    AUDIO_ENCODER_PCM = 6,//PCM support added for wifi display feature
+#endif
     AUDIO_ENCODER_LIST_END // must be the last - used to validate the audio encoder type
 };
 
